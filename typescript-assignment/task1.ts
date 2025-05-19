b97947ae4b76123dc0db706fc25f8aa4e6dce899
arka-work <swdevintern2@chargeworks.in>
2025-05-19 17:18:09 +0530

    Typed Variable Declaration

---
+++ typescript-assignment/task1.ts
@@ -0,0 +1,19 @@
+let fullname:string="arka prava kotal";
+
+let age:number=21;
+
+let istrue:boolean=false;
+
+let colors: string[] = ["red", "green", "blue"];
+
+let run: number[] = [85, 92, 78];
+
+interface Person{
+    ename :string;
+    age :number;
+}
+
+let person1 : Person={
+    ename :"ani",
+    age :22
+};
